a26b0466972677cf2d3ea2a3b202e99a942b816e
Evan Denmark <edenmark@mit.edu>
2014-10-04 14:45:33 -0400

    Update Constants.java

--- MainActivity/src/com/example/gatas/Constants.java
+++ MainActivity/src/com/example/gatas/Constants.java
@@ -19,7 +19,16 @@ public interface Constants extends BaseColumns{
                 AUTHORITY + "/" + EVENT_TABLE);
                 
     public static final point pocket1= new point();
-    public static final point pocket1= new point();
-    public static final point pocket1= new point();
+    public static final point pocket2= new point();
+    public static final point pocket3= new point();
+    public static final point pocket4= new point();
+    public static final point pocket5= new point();
+    public static final point pocket6= new point();
+    public static final float tablelenx = //Define table dimensions
+    public static final float tableleny = //Deine table dim
+    public static final float radius = //define radius
+    public static final float deceleration = //Define deceleration/friction constant
+    
+    
 
 }
